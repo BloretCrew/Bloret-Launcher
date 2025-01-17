2b5af92df8fb75dd9610bc0516bd37148a6fe5a1
Detrital <Detritalw@outlook.com>
2025-01-17 11:47:34 +0800

    Delete comboBox.py

--- comboBox.py
+++
@@ -1,8 +0,0 @@
-comboBox = ComboBox()
-
-# 添加选项
-items = ['shoko', '西宫硝子', '宝多六花', '小鸟游六花']
-comboBox.addItems(items)
-
-# 当前选项的索引改变信号
-comboBox.currentIndexChanged.connect(lambda index: print(comboBox.currentText()))
