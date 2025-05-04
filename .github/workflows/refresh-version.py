63abf4edbaaa89c500552a0c610dff1868dbf74e
Detritalw <Detritalw@outlook.com>
2025-05-04 08:57:43 +0800

    ci : 自动更新 cmcl.json 文件

--- .github/workflows/refresh-version.py
+++ .github/workflows/refresh-version.py
@@ -1,5 +1,8 @@
 import json
 
+'''
+修改 config.json 文件中的指定字段
+'''
 # 读取 config.json 文件
 with open('config.json', 'r', encoding='utf-8') as config_file:
     config = json.load(config_file)
@@ -17,6 +20,35 @@ config['whenCWopen_BLopen'] = True
 config['home_show_login_mod'] = True
 config['localmod'] = False
 
+# 保存修改后的 config.json
+with open('config.json', 'w', encoding='utf-8') as config_file:
+    json.dump(config, config_file, indent=4)
+
+print("Config values updated successfully.")
+
+'''
+修改 cmcl.json 文件中的指定字段
+'''
+# 读取 cmcl.json 文件
+with open('cmcl.json', 'r', encoding='utf-8') as cmcl_file:
+    cmcl = json.load(cmcl_file)
+
+# 修改 cmcl.json 的指定字段
+cmcl['language-run'] = "zh"
+cmcl['downloadSource'] = 1
+cmcl['checkAccountBeforeStart'] = False
+cmcl['accounts'] = []
+cmcl['printStartupInfo'] = True
+
+# 保存修改后的 cmcl.json
+with open('cmcl.json', 'w', encoding='utf-8') as cmcl_file:
+    json.dump(cmcl, cmcl_file, indent=4)
+
+print("CMCL values updated successfully.")
+
+'''
+修改 Bloret-Launcher-Setup.iss 文件中的 MyAppVersion 字段
+'''
 # 获取 ver 值并转换为字符串
 version = config.get('ver', '')
 version_str = str(version)
@@ -37,8 +69,3 @@ with open(setup_file_path, 'w', encoding='utf-8') as setup_file:
     setup_file.writelines(setup_content)
 
 print(f"Updated MyAppVersion to {version_str} in {setup_file_path}")
-# 保存修改后的 config.json
-with open('config.json', 'w', encoding='utf-8') as config_file:
-    json.dump(config, config_file, indent=4)
-
-print("Config values updated successfully.")
