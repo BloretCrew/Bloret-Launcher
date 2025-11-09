542e49865fceb316073b1687c7d16bd96dd6956a
Detritalw <Detritalw@outlook.com>
2025-11-09 14:15:39 +0800

    up

--- Bloret-Launcher.py
+++ Bloret-Launcher.py
@@ -14,6 +14,7 @@ from modules.log import log
 from modules.systems import get_system_theme_color,is_dark_theme,check_write_permission,restart,setup_startup_with_self_starting
 from modules.setup_ui import setup_home_ui,setup_download_old_ui,setup_tools_ui,setup_passport_ui,setup_settings_ui,setup_info_ui,load_ui,setup_version_ui,setup_BBS_ui, setup_Mod_ui, setup_multiplayer_ui, setup_download_ui
 from modules.customize import CustomizeRun
+from modules.global_hotkey import init_global_hotkeys
 from modules.BLServer import check_Light_Minecraft_Download_Way,handle_first_run,check_Bloret_version,check_for_updates
 from modules.links import open_BBBS_link
 from modules.BLDownload import BL_download
@@ -333,6 +334,9 @@ class MainWindow(FluentWindow):
         # 初始化托盘图标
         self.tray_icon = SystemTrayIcon(parent=self)
         self.tray_icon.show()
+
+        # 初始化全局快捷键
+        init_global_hotkeys()
 
         # 处理首次运行
         update_progress({'value': 70 / 100, 'valueStringOverride': '7/10', 'status': i18nText('处理首次运行')})
